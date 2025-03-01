ab33acf66b9b396d47e333ae34b07b3c5c8f0267
thejope <joaomonte19761@gmail.com>
2025-03-01 01:34:35 -0300

    unificando a forma de usar biblioteca no footer e na header

---
+++ config/library/lib_header.js
@@ -0,0 +1,19 @@
+// Função para adicionar um link CSS ao head
+function addStylesheet(href, integrity = '', crossorigin = '') {
+    let link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = href;
+    if (integrity) link.integrity = integrity;
+    if (crossorigin) link.crossOrigin = crossorigin;
+    document.head.appendChild(link);
+}
+
+// Adicionando os estilos ao head
+addStylesheet('css/estrutura.css');
+addStylesheet('https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+    'sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH', 'anonymous');
+addStylesheet('https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css');
+addStylesheet('css/owl.carousel.min.css');
+addStylesheet('css/owl.theme.default.min.css');
+addStylesheet('css/loader.css');
+addStylesheet('https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css');
